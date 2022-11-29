e23c8b42fab0f828bd972ebcbffa2eb26c04630a
Ekaterina <108363825+mckater@users.noreply.github.com>
2022-11-29 15:59:57 +0300

    red_circle_to_right

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import pygame
 
-if __name__ == '__main__':
-    pygame.init()
-    size = width, height = 800, 400
-    screen = pygame.display.set_mode(size)
+size = width, height = 800, 400
+screen = pygame.display.set_mode(size)
 
-    running = True
-    while running:
-        # внутри игрового цикла ещё один цикл
-        # приема и обработки сообщений
-        for event in pygame.event.get():
-            # при закрытии окна
-            if event.type == pygame.QUIT:
-                running = False
+running = True
+x_pos = 0
+while running:
+    # внутри игрового цикл еще один цикл
+    # приема и обработки сообщений
+    events = pygame.event.get()
+    for event in events:
 
-        # отрисовка и изменение свойств объектов
-        # ...
+        # при закрытии окна
+        if event.type == pygame.QUIT:
+            running = False
 
-        # обновление экрана
-        pygame.display.flip()
-    pygame.quit()
+    # отрисовка и изменение свойств объектов
+    # ...
+    screen.fill((0, 0, 0))
+    pygame.draw.circle(screen, (255, 0, 0), (x_pos, 200), 20)
+    x_pos += 1
+
+    # обновление экрана
+    pygame.display.flip()
+
+pygame.quit()
