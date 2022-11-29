a4e4238b04ae21bbaaefb0dcdd9323dbe0564174
Ekaterina <108363825+mckater@users.noreply.github.com>
2022-11-29 16:06:11 +0300

    tv_snow

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 import pygame
+import random
+
+
+def draw():
+    for i in range(10000):
+        screen.fill(pygame.Color('white'), (random.random() * width, random.random() * height, 1, 1))
+
 
 size = width, height = 800, 400
 screen = pygame.display.set_mode(size)
 
 running = True
-x_pos = 0
 while running:
     # внутри игрового цикл еще один цикл
     # приема и обработки сообщений
-    events = pygame.event.get()
-    for event in events:
-
+    for event in pygame.event.get():
         # при закрытии окна
         if event.type == pygame.QUIT:
             running = False
 
     # отрисовка и изменение свойств объектов
-    # ...
     screen.fill((0, 0, 0))
-    pygame.draw.circle(screen, (255, 0, 0), (x_pos, 200), 20)
-    x_pos += 1
+    draw()
 
     # обновление экрана
     pygame.display.flip()
