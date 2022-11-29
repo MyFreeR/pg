15553f468857d19a9afa1c4f5050f34fdca7ee1b
Ekaterina <108363825+mckater@users.noreply.github.com>
2022-11-29 16:51:39 +0300

    moving

--- main.py
+++ main.py
@@ -1,45 +1,50 @@
 import pygame
 
-size = width, height = 400, 300
+size = width, height = 300, 300
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-pygame.display.set_caption('Шарики')
+pygame.display.set_caption('Перетаскивание')
 running = True
-# параметра круга
+# задаем параметры квадрата
+rect_width = 0
+rect_x = 0
+rect_y = 0
+square_width = 100
+rect_color = pygame.Color('green')
+rect_rect = ((rect_x, rect_y), (square_width, square_width))
+x1 = x2 = y1 = y2 = 0
 
-circle_radius = 10
-circle_color = pygame.Color('white')
-# Пустые списки для координат и скоростей наших кругов
-circle = []
-speed = []
-# Создаем второй холст
-screen2 = pygame.Surface(screen.get_size())
-# Запускаем окно
 while running:
+    screen.fill(pygame.Color('black'))
+
     for event in pygame.event.get():
-        screen2 = pygame.Surface(screen.get_size())
         if event.type == pygame.QUIT:
             running = False
+
+        # реакция на нажатие мыши
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # Сохраняем координаты курсора
+            x1, y1 = event.pos
+            # если курсор не попал в область квадрата, координаты обнуляем
+            if (x1 < rect_x) or (x1 > rect_x + square_width) or (y1 < rect_y) or (y1 > rect_y + square_width):
+                x1 = y1 = 0
+
+        # реакция на отжатие кнопки мыши
         if event.type == pygame.MOUSEBUTTONUP:
-            # Сохраняем полученные координаты
-            circle.append(list(event.pos))
-            speed.append([-1, -1])
+            # Сохраняем новые координаты вершины квадрата
+            rect_x += x2
+            rect_y += y2
+            # обнуляем смещение координат
+            x1 = x2 = y1 = y2 = 0
+        # реакция на движение мышки с условием того, что кнопка зажата
+        if event.type == pygame.MOUSEMOTION and x1 > 0:
+            # считаем смещение координат
+            x2 = event.pos[0] - x1
+            y2 = event.pos[1] - y1
+    # рисуем прямоугольник
+    rect_rect = ((rect_x + x2, rect_y + y2), (square_width, square_width))
+    pygame.draw.rect(screen, rect_color, rect_rect, rect_width)
 
-    # Чистим второй холст
-    screen2.fill(pygame.Color('black'))
-    # Перерисовываем круги
-    for i in range(len(circle)):
-        # Проверяем на столкновение со стенками
-        for ext in (0, 1):
-            if circle[i][ext] >= size[ext] - circle_radius or circle[i][ext] <= circle_radius:
-                speed[i][ext] = -speed[i][ext]
-            # Изменяем координаты
-            circle[i][ext] += speed[i][ext]
-        # Рисуем круг
-        pygame.draw.circle(screen2, circle_color, circle[i], circle_radius, 0)
-
-    # Рисуем на экране сохраненное на втором холсте
-    screen.blit(screen2, (0, 0))
     pygame.display.flip()
-    clock.tick(100)
+    clock.tick(50)
 pygame.quit()
