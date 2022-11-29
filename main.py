ce618476dd5e6044a5a273310837643543c9e5f4
Ekaterina <108363825+mckater@users.noreply.github.com>
2022-11-29 16:38:35 +0300

    balls

--- main.py
+++ main.py
@@ -3,34 +3,43 @@ import pygame
 size = width, height = 400, 300
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-pygame.display.set_caption('Желтый круг')
+pygame.display.set_caption('Шарики')
 running = True
-# Красим фон
-screen.fill(pygame.Color('blue'))
-circle_width = 0
-circle_radius = 0
-circle_exists = False  # переменная указывает на существование круга
-circle_color = pygame.Color('yellow')
-plus_radius = pygame.USEREVENT + 25  # номер нашего созданного события
-# таймер вызова события увеличения радиуса круга
-pygame.time.set_timer(plus_radius, 10)
+# параметра круга
 
+circle_radius = 10
+circle_color = pygame.Color('white')
+# Пустые списки для координат и скоростей наших кругов
+circle = []
+speed = []
+# Создаем второй холст
+screen2 = pygame.Surface(screen.get_size())
+# Запускаем окно
 while running:
     for event in pygame.event.get():
+        screen2 = pygame.Surface(screen.get_size())
         if event.type == pygame.QUIT:
             running = False
-        # реакция на нажатие мыши
         if event.type == pygame.MOUSEBUTTONUP:
-            circle_exists = True
-            # красим фон и поверх него рисуем новый круг
-            screen.fill(pygame.Color('blue'))
-            circle_radius = 0
-            circle_pos = event.pos
-            pygame.draw.circle(screen, circle_color, circle_pos, circle_radius, circle_width)
-            # Событие увеличения радиуса круга
-        if event.type == plus_radius and circle_exists:
-            circle_radius += 1
-            pygame.draw.circle(screen, circle_color, circle_pos, circle_radius, circle_width)
+            # Сохраняем полученные координаты
+            circle.append(list(event.pos))
+            speed.append([-1, -1])
+
+    # Чистим второй холст
+    screen2.fill(pygame.Color('black'))
+    # Перерисовываем круги
+    for i in range(len(circle)):
+        # Проверяем на столкновение со стенками
+        for ext in (0, 1):
+            if circle[i][ext] >= size[ext] - circle_radius or circle[i][ext] <= circle_radius:
+                speed[i][ext] = -speed[i][ext]
+            # Изменяем координаты
+            circle[i][ext] += speed[i][ext]
+        # Рисуем круг
+        pygame.draw.circle(screen2, circle_color, circle[i], circle_radius, 0)
+
+    # Рисуем на экране сохраненное на втором холсте
+    screen.blit(screen2, (0, 0))
     pygame.display.flip()
-    clock.tick(50)
+    clock.tick(100)
 pygame.quit()
