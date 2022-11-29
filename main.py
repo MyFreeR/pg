fdd4ac158cda61f196e01168597a7d83e0a3bb79
Ekaterina <108363825+mckater@users.noreply.github.com>
2022-11-29 16:17:32 +0300

    blue_ball

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 import pygame
-# clear
+
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEMOTION:
+            pygame.draw.circle(screen, (0, 0, 255), event.pos, 20)
+
+    pygame.display.flip()
+    clock.tick(50)
+
+pygame.quit()
