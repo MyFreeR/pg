7070b438cb6d6e668d4d7fa75ec453d63b362b42
Ekaterina <108363825+mckater@users.noreply.github.com>
2022-12-03 15:06:51 +0300

    lesson_3_get_cell

--- main.py
+++ main.py
@@ -18,6 +18,24 @@ class Board:
         self.top = top
         self.cell_size = cell_size
 
+    def get_cell(self, mouse_pos):
+        cell_x = (mouse_pos[0] - self.left) // self.cell_size
+        cell_y = (mouse_pos[1] - self.top) // self.cell_size
+        if cell_x < 0 or cell_x >= self.width or cell_y < 0 or cell_y >= self.height:
+            return None
+        return cell_x, cell_y
+
+    # cell - кортеж (x, y)
+    def on_click(self, cell):
+        print(cell)
+
+    def get_click(self, mouse_pos):
+        cell = self.get_cell(mouse_pos)
+        if cell:
+            self.on_click(cell)
+        else:
+            print(cell)  # которая None
+
     def render(self, screen):
         for y in range(self.height):
             for x in range(self.width):
@@ -38,6 +56,8 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                board.get_click(event.pos)
         screen.fill((0, 0, 0))
         board.render(screen)
         pygame.display.flip()
